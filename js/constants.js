5a30d6fbd105a7c4fee36d8091533e1a1fe74d0d
pcmquizon <camillemendoza17@gmail.com>
2017-03-16 15:53:54 +0800

    update UI

---
+++ js/constants.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const tablePath = {
+  coursec: 'tables/COURSEC.csv',
+  courseoffering: 'tables/COURSEOFFERING.csv',
+  studcourse: 'tables/STUDCOURSE.csv',
+  student: 'tables/STUDENT.csv',
+  studenthistory: 'tables/STUDENTHISTORY.csv',
+}
